4ca47d0e04be0f01c7456a49f56199530b406914
Matthew Kleimeyer <matthewkleimeyer@gmail.com>
2022-03-28 21:26:49 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,29 @@
 #bot.py
 import os
 import discord
+import json
+import requests
+from graphqlclient import GraphQLClient
 from dotenv import load_dotenv
 from discord.ext import commands
+#from qlient import Client, GraphQLResponse
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+SMASHTOKEN = os.getenv('SMASHGG_TOKEN')
+ggApiVersion = 'alpha'
+
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='helloBot')
-async def navi(ctx):
-    await ctx.send('hey!')
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
 
-bot.run(TOKEN)
+
+# @bot.command(name='helloBot')
+# async def navi(ctx):
+#     await ctx.send('hey!')
+
+# @bot.event
+# async def on_ready():
+#     print(f'{bot.user.name} has connected to Discord!')
+
+# bot.run(TOKEN)
