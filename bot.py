106cfe3f18a5564ed098be5bd55c4710e7891d49
Matthew Kleimeyer <matthewkleimeyer@gmail.com>
2022-03-28 22:55:35 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,15 +15,73 @@ ggApiVersion = 'alpha'
 
 bot = commands.Bot(command_prefix='!')
 
+gql = GraphQLClient('https://api.smash.gg/gql/' + ggApiVersion)
+gql.inject_token('Bearer ' + SMASHTOKEN)
 
 
 
-# @bot.command(name='helloBot')
-# async def navi(ctx):
-#     await ctx.send('hey!')
+@bot.command(name = 'standings')
+async def cmdstandings(ctx, tourneyslug, amount: int):
+    getTourneyBySlug = gql.execute('''
+    query TournamentInfo($slug: String!) {
+    tournament(slug: $slug) {
+        id
+        name
+        events {
+        id
+        }
+    }
+    }
+    ''',
+    {
+        "slug": tourneyslug
+    })
+    tourney = json.loads(getTourneyBySlug)
+    eventID = tourney["data"]["tournament"]["events"][0]["id"]
 
-# @bot.event
-# async def on_ready():
-#     print(f'{bot.user.name} has connected to Discord!')
 
-# bot.run(TOKEN)
+
+    getEventStandings = gql.execute('''
+    query EventStandings($eventId: ID!, $page: Int!, $perPage: Int!) {
+    event(id: $eventId) {
+        id
+        name
+        standings(query: {
+        perPage: $perPage,
+        page: $page
+        }){
+        nodes {
+            placement
+            entrant {
+            id
+            name
+            }
+        }
+        }
+    }
+    }
+    ''',
+    {
+        "eventId": eventID,
+        "page": 1,
+        "perPage": amount
+    })
+
+    standings = json.loads(getEventStandings)
+    message = f'Top {amount} for {standings["data"]["event"]["name"]}:\n'
+    for _standing in standings["data"]["event"]["standings"]["nodes"]:
+        message += f'{_standing["placement"]}: {_standing["entrant"]["name"]}\n'
+    await ctx.send(message)
+
+
+
+
+@bot.command(name='helloBot')
+async def navi(ctx):
+    await ctx.send('hey!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+bot.run(TOKEN)
